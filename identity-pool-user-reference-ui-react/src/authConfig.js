d6da74d1c7243c8beca867969a5abee341fd7d8f
Caleb Sattgast <csattgast@cloudentity.com>
2022-06-01 17:54:59 -0700

    Frontend app auth config refinements

--- identity-pool-user-reference-ui-react/src/authConfig.js
+++ identity-pool-user-reference-ui-react/src/authConfig.js
@@ -1,4 +1,5 @@
  const authConfig = {
+      // Basic ACP authorization flow configs:
       domain: 'localhost:8443', // e.g. 'example.authz.cloudentity.io' (domain only, without 'https://' prefix)
       tenantId: 'default', // If using ACP SaaS, this is generally in the subdomain of your ACP SaaS URL. For local ACP, value is 'default'
       authorizationServerId: 'login-demo', // This is generally the name of the workspace you created the OAuth application in.
@@ -7,8 +8,15 @@
       scopes: ['profile', 'email', 'openid'], // 'revoke_tokens' scope must be present for 'logout' action to revoke token. Without it, token will only be deleted from browser's local storage.
       accessTokenName: 'identity_demo_access_token',
       idTokenName: 'identity_demo_id_token',
-      nodeJsBackendEnabled: false, // Set to 'true' if 'identity-pool-reference-ui-services-nodejs' is set up and running. When set to 'false,' custom schema will not be fetched for self-update profile action.
-      customLoginEnabled: false // Set to 'true' if 'identity-pool-reference-ui-authn-app-nodejs' is set up and running, and a custom IDP has been set up for your ACP workspace.
+
+      // App functionality configs:
+      nodeJsBackendEnabled: false,
+      // Set to 'true' if 'identity-pool-reference-ui-services-nodejs' is set up and running, and a custom user schema is being utilized.
+      // When set to 'false,' custom user schema will not be utilized for profile functionality, even if one exists for the configured identity pool.
+
+      customLoginEnabled: false
+      // Set to 'true' if 'identity-pool-reference-ui-authn-app-nodejs' is set up and running, and a custom IDP has been set up for the ACP workspace.
+      // When set to 'false,' direct requests to ACP '/self/me' APIs will be used for profile functionality.
   };
 
  export default authConfig;
