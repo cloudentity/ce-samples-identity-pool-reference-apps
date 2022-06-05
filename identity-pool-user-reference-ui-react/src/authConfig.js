59eb78dfc9de64f9a6a132e56166ca11fa39564f
Caleb Sattgast <csattgast@cloudentity.com>
2022-06-04 22:10:50 -0700

    Upgrade @cloudentity/auth library in user frontend app -> 2.0.2-beta.0

--- identity-pool-user-reference-ui-react/src/authConfig.js
+++ identity-pool-user-reference-ui-react/src/authConfig.js
@@ -6,6 +6,7 @@
       clientId: 'login-demo', // Find this value by viewing the details of your OAuth application
       redirectUri: 'http://localhost:3000/', // Make sure to add this exact value (including trailing slash) to the 'redirect_uri' list of your OAuth application settings
       scopes: ['profile', 'email', 'openid'], // 'revoke_tokens' scope must be present for 'logout' action to revoke token. Without it, token will only be deleted from browser's local storage.
+      responseType: ['code'],
       accessTokenName: 'identity_demo_access_token',
       idTokenName: 'identity_demo_id_token',
 
